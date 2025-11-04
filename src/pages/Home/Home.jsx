84927a90115e992e2a7806bd60dd463b8ecfe968
unknown <blackskiesdancer@gmail.com>
2025-11-04 15:50:38 +1100

    updated fonts

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,9 +3,10 @@ import BubbleGrid from "../../components/BubbleGrid";
 export default function Home() {
   return (
     <section className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-5xl font-bold text-ink">Shan Lowe</h1>
-      <p className="opacity-80 mt-2">Developer • Creative • Advocate</p>
-
+      <div className="text-left">
+      <h1 className="text-5xl text-ink">Shannon Lowe</h1>
+      <p className="opacity-80 mt-2 custom-font">Sydney, Australia - She/Her</p>
+      </div>
       <BubbleGrid />
     </section>
   );
